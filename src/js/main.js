a663d20be761e3fb0bc47a2451ff474ac921c5df
torexxx <molento-studio@yandex.ru>
2021-03-14 23:16:37 +0700

    video player

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,10 @@
 import Slider from './modules/slider';
+import VideoPlayes from './modules/playVideo';
 
 window.addEventListener('DOMContentLoaded', () => {
     const slider = new Slider('.page', '.next');
     slider.render();
+    const video = new VideoPlayes('.showup .play', '.overlay');
+    video.init();
+
 })
